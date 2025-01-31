21e2e54137088ff272671e47379af2bd796b6e3f
rafaavmsilva <rafaavmsilva@gmail.com>
2025-01-31 14:34:55 -0300

    merge updates

--- app.py
+++ app.py
@@ -1247,4 +1247,5 @@ def extract_and_enrich_cnpj(description, transaction_type):
     return description
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host='0.0.0.0', port=port, debug=False)
